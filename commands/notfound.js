f82b4a6c97ab7f9ae4bdaed48723a3d8b4fa805c
Roy Dupaya <friedroyze@gmail.com>
2020-11-07 19:44:37 +0800

    added more help info for notfound.js

--- commands/notfound.js
+++ commands/notfound.js
@@ -4,5 +4,14 @@ module.exports.run = async (bot, message, args) => {
 
 module.exports.help = {
     name: "notfound",
-    description: "informs user the command does not exist"
+    aliases: ["notfound"],
+    description: "informs user the command does not exist",
+    
+    requiredArgs: "none",
+    minArgs: 0,
+    maxArgs: null,
+
+    permissions:[],
+    requredRoles:[],
+    permissionError: "you do not have perms to use this",
 }

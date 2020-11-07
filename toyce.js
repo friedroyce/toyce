4a152a3c63d023b11daa6e0f4f5993f6b274b718
Roy Dupaya <friedroyze@gmail.com>
2020-11-07 20:36:40 +0800

    improved command handler

--- toyce.js
+++ toyce.js
@@ -1,14 +1,42 @@
 const discord = require('discord.js')
-const config = require('./config.json')
+const {prefix, token} = require('./config.json')
+const fs = require('fs')
+const notfound = require('./commands/notfound')
 
 const bot = new discord.Client()
+bot.commands = new discord.Collection()
 
-bot.login(config.token)
+const cmds = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+for(const file of cmds){
+    const cmd = require(`./commands/${file}`)
+    bot.commands.set(cmd.name, cmd)
+    console.log(`loaded command '${cmd.name}'`)
+}
 
+//when toyce goes online
 bot.once('ready', () => {
     console.log('toyce is online!')
 })
 
-bot.on('message', (message) => {
-    if(!message.content.startsWith(config.prefix) || message.author.bot) return;
+//message event
+bot.on('message', async (message) => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return
+
+    const args = message.content.slice(prefix.length).split(" ")
+    const cmdName = args.shift().toLowerCase()
+
+    const command = bot.commands.get(cmdName)
+        || bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(cmdName))
+
+    if(!command) return bot.commands.get('notfound').execute(bot, message, args)
+
+    try{
+        command.execute(bot, message, args)
+    }
+    catch(e){
+        message.channel.send("an error occured executing the command")
+        console.log(e)
+    }
 })
+
+bot.login(token)

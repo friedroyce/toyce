78445ad93a8c9153a0df8dc40de26f3d377ad47f
Roy Dupaya <friedroyze@gmail.com>
2020-11-08 16:49:12 +0800

    changed ping calculation

--- commands/information/ping.js
+++ commands/information/ping.js
@@ -4,12 +4,10 @@ module.exports = {
     category: 'information',
     aliases: ['ping', 'kaping', 'ka-ping', 'badaping', 'ba-da-ping', 'bada-ping'],
     run: async (bot, message, args) => {
-        message.channel.send('...').then(msg => {
-            var pong = ['pong', 'ka-pong', 'ba-da-pong', 'bada-pong']
-            const ping = msg.createdTimestamp - message.createdTimestamp
-            const i = Math.floor((Math.random() * pong.length))
-
-            msg.edit(`${pong[i]} (${ping}ms)`)
-        })
+        var pong = ['pong', 'ka-pong', 'ba-da-pong', 'bada-pong']
+        const i = Math.floor((Math.random() * pong.length))
+        
+        const ping = Date.now() - message.createdTimestamp
+        message.channel.send(`${pong[i]}! your ping is ${ping}ms`)
     }
 }

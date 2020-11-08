e3c0f792fd7935ddd21b7641736f6dc378135dd0
Roy Dupaya <friedroyze@gmail.com>
2020-11-08 16:49:25 +0800

    added meme command

---
+++ commands/shitpost/meme.js
@@ -0,0 +1,24 @@
+const discord = require('discord.js')
+const imageapi = require('imageapi.js')
+
+module.exports = {
+    name: 'meme',
+    description: 'sends a meme',
+    category: 'shitpost',
+    aliases: ['meme'],
+    run: async (bot, message, args) => {
+        let subreddits = [
+            "memes"
+        ]
+        let subreddit = subreddits[Math.floor(Math.random()*(subreddits.length))]
+        let img = await imageapi(subreddit)
+        const embed = new discord.MessageEmbed()
+        // .setTitle(`A meme from r/${subreddit}`)
+        // .setURL(`https://www.reddit.com/r/${subreddit}`)
+        .setColor('RANDOM')
+        .setImage(img)
+        message.channel.send(embed)
+
+        //message.channel.send(img)
+    }
+}

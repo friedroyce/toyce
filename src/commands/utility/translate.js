9c67122e2bcfbc6046aa3dc8921a54aa985c73e9
Roy Dupaya <friedroyze@gmail.com>
2020-11-11 02:43:48 +0800

    moved files

---
+++ src/commands/utility/translate.js
@@ -0,0 +1,14 @@
+const discord = require('discord.js')
+const superagent = require('superagent')
+
+module.exports = {
+    name: 'translate',
+    description: 'sends lyrics for a song',
+    category: 'utility',
+    aliases: ['translate'],
+    run: async (bot, message, args) => {
+
+        
+        
+    }
+}

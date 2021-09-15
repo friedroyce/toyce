508cc671c64d195d525ad588793d8a950847f8bd
Roy <friedroyze@gmail.com>
2021-09-15 08:50:35 +0800

    changed to translate

--- commands/utility/translate.js
+++ commands/utility/translate.js
@@ -2,10 +2,10 @@ const discord = require('discord.js')
 const superagent = require('superagent')
 
 module.exports = {
-    name: 'lyrics',
+    name: 'translate',
     description: 'sends lyrics for a song',
     category: 'utility',
-    aliases: ['ly'],
+    aliases: ['translate'],
     run: async (bot, message, args) => {
 
         

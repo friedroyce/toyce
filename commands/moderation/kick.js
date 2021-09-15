6a4f8b9a0d9d43e5657e54e40b34548dc14c4274
Roy <friedroyze@gmail.com>
2021-09-15 22:43:25 +0800

    added kick

---
+++ commands/moderation/kick.js
@@ -0,0 +1,41 @@
+const Discord = require('discord.js')
+const {prefix, colour} = require('../../config.json')
+
+module.exports = {
+    name: 'kick',
+    description: 'kicks a user from the server',
+    category: 'moderation',
+    aliases: ['kick', 'kik'],
+    run: async (bot, message, args) => {
+        if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("You cannot use this command");
+
+        const mentionedMember = message.mentions.members.first();
+        let reason = args.slice(1).join(" ");
+
+        if(!reason) reason = "for no reason. that's sus";
+
+        const kickEmbed = new Discord.MessageEmbed()
+            .setTitle(`you just got yeeted from ${message.guild.name}`)
+            .setDescription(`Reason: ${reason}`)
+            .setColor(colour)
+            .setTimestamp()
+            .setFooter(client.user.tag, client.user.displayAvatarURL());
+
+        if(!args) return message.channel.send(`usage: \`${prefix}kick <@user> <reason>\``);
+        if(!mentionedMember) return message.channel.send(`that user is not a member of the server`);
+
+        try{
+            await mentionedMember.send(kickEmbed);
+        } catch(err){
+            console.log(`was unable to message the member`);
+        }
+
+        try{
+            await mentionedMember.kick(reason);
+            message.channel.send("user got successfully yeeted");
+        } catch(err){
+            console.log(err);
+            message.channel.send("was unable to yeet the user");
+        }
+    }
+}

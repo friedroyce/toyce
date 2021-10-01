b6d1dd41f6a72dfa737d1decb47788e46cb31f25
Roy <friedroyze@gmail.com>
2021-10-02 04:26:30 +0800

    reworked command handler

--- src/handlers/command.js
+++ src/handlers/command.js
@@ -1,34 +1,72 @@
-const fs = require('fs')
 const path = require('path')
 
-module.exports.setup = (bot) => {
+const { REST } = require('@discordjs/rest')
+const { Routes } = require('discord-api-types/v9')
+const fs = require('fs')
 
-    const commandfiles = findNested('./commands', '.js')
+const clientId = '866679101916839977'
+const guildId = '849691145830137919'
 
-    if(commandfiles.length <= 0) return console.log("there are no commands to load")
+module.exports = (client) => {
+    client.loadCommands = async (path) => {
+        client.commandArray = []
+        const commandfiles = findNested(path, '.js')
+        if (commandfiles.length <= 0) return console.log("there are no commands to load")
+        console.log(`loading commands`)
+        commandfiles.forEach(file => {
+            const command = require(file)
+            client.commands.set(command.data.name, command)
+            client.commandArray.push(command.data.toJSON())
+            console.log(`loaded command '${command.data.name}'`)
+        })
 
-    console.log(`loading ${commandfiles.length} commands`)
-    commandfiles.forEach(file => {
-        const commandfile = require(file)
-        bot.commands.set(commandfile.name, commandfile)
+        const rest = new REST({ version: '9' }).setToken(process.env.token);
 
-        console.log(`loaded command '${commandfile.name}'`)
-    })
+        (async () => {
+            try {
+                console.log('Started refreshing application (/) commands.')
 
+                await rest.put(
+                    Routes.applicationGuildCommands(clientId, guildId),
+                    { body: client.commandArray },
+                );
+
+                console.log('Successfully reloaded application (/) commands.')
+            } catch (error) {
+                console.error(error)
+            }
+        })()
+    }
 }
 
-function findNested(dir, pattern){
+// module.exports.setup = (bot) => {
+
+//     const commandfiles = findNested('./commands', '.js')
+
+//     if(commandfiles.length <= 0) return console.log("there are no commands to load")
+
+//     console.log(`loading ${commandfiles.length} commands`)
+//     commandfiles.forEach(file => {
+//         const commandfile = require(file)
+//         bot.commands.set(commandfile.name, commandfile)
+
+//         console.log(`loaded command '${commandfile.name}'`)
+//     })
+
+// }
+
+function findNested(dir, pattern) {
     let results = []
 
     fs.readdirSync(dir).forEach(subdir => {
         subdir = path.resolve(dir, subdir)
         const stat = fs.statSync(subdir)
 
-        if(stat.isDirectory()){
+        if (stat.isDirectory()) {
             results = results.concat(findNested(subdir, pattern))
         }
 
-        if(stat.isFile() && subdir.endsWith(pattern)){
+        if (stat.isFile() && subdir.endsWith(pattern)) {
             results.push(subdir)
         }
 

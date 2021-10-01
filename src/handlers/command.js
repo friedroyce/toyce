cb9f078d2de2269ab7f077ad1a2aa4dc5019c023
Roy <friedroyze@gmail.com>
2021-10-02 04:27:20 +0800

    deleted comments

--- src/handlers/command.js
+++ src/handlers/command.js
@@ -1,5 +1,4 @@
 const path = require('path')
-
 const { REST } = require('@discordjs/rest')
 const { Routes } = require('discord-api-types/v9')
 const fs = require('fs')
@@ -39,37 +38,14 @@ module.exports = (client) => {
     }
 }
 
-// module.exports.setup = (bot) => {
-
-//     const commandfiles = findNested('./commands', '.js')
-
-//     if(commandfiles.length <= 0) return console.log("there are no commands to load")
-
-//     console.log(`loading ${commandfiles.length} commands`)
-//     commandfiles.forEach(file => {
-//         const commandfile = require(file)
-//         bot.commands.set(commandfile.name, commandfile)
-
-//         console.log(`loaded command '${commandfile.name}'`)
-//     })
-
-// }
-
 function findNested(dir, pattern) {
     let results = []
 
     fs.readdirSync(dir).forEach(subdir => {
         subdir = path.resolve(dir, subdir)
         const stat = fs.statSync(subdir)
-
-        if (stat.isDirectory()) {
-            results = results.concat(findNested(subdir, pattern))
-        }
-
-        if (stat.isFile() && subdir.endsWith(pattern)) {
-            results.push(subdir)
-        }
-
+        if (stat.isDirectory()) results = results.concat(findNested(subdir, pattern))
+        if (stat.isFile() && subdir.endsWith(pattern)) results.push(subdir)
     })
 
     return results

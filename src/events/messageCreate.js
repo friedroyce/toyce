a809e65b0c03d3b6c62f4684f40dcdd666c9dfc9
Roy <friedroyze@gmail.com>
2021-10-02 04:28:05 +0800

    deleted comments

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -31,34 +31,3 @@ module.exports = {
         }
     }
 }
-
-// bot.on('messageCreate', async (message) => {
-//     const args = message.content.slice(prefix.length).split(" ")
-    
-
-//     if(message.content.startsWith(prefix) && !message.author.bot) {
-//         const cmdName = args.shift().toLowerCase()
-//         const command = bot.commands.get(cmdName)
-//             || bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(cmdName))
-
-//         if(!command) return 
-
-//         if (!message.channel.nsfw && command.category === 'nsfw') {
-//             message.react('💢');
-      
-//             return message.reply('dis not an NSFW channel')
-//             .then(msg => {
-//                 msg.delete({ timeout: 3000 })
-//             })
-//         }
-
-//         try{
-//             command.run(bot, message, args)
-//         }
-//         catch(e){
-//             message.channel.send("an error occured executing the command")
-//             console.log(e)
-//         }
-//     }
-
-// })

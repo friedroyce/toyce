afb3213082b6e0b81ac70c51c474d76514bffc26
Roy <friedroyze@gmail.com>
2021-10-02 04:30:42 +0800

    deleted comments

--- src/commands/utility/ping.js
+++ src/commands/utility/ping.js
@@ -18,18 +18,3 @@ module.exports = {
 		}
 	}
 }
-
-
-// module.exports = {
-//     name: 'ping',
-//     description: 'checks the bots latency',
-//     category: 'utility',
-//     aliases: ['ping', 'kaping', 'ka-ping', 'badaping', 'ba-da-ping', 'bada-ping'],
-//     run: async (bot, message, args) => {
-//         var pong = ['pong', 'ka-pong', 'ba-da-pong', 'bada-pong']
-//         const i = Math.floor((Math.random() * pong.length))
-
-//         const ping = Date.now() - message.createdTimestamp
-//         message.channel.send(`${pong[i]}! your ping is ${ping}ms`)
-//     }
-// }

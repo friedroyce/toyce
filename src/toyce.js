4b57d0c7d808ab2f7743b12f6e3255dc05eaef39
Roy <friedroyze@gmail.com>
2021-11-18 20:06:46 +0800

    created snipes array

--- src/toyce.js
+++ src/toyce.js
@@ -3,7 +3,9 @@ const fs = require('fs')
 const { Client, Intents, Collection } = require('discord.js')
 const handlers = fs.readdirSync('./src/handlers').filter(file => file.endsWith('.js'))
 const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"], partials: ["CHANNEL"] })
+
 client.commands = new Collection();
+client.snipes = {};
 
 (async () => {
     handlers.forEach(handler => { require(`./handlers/${handler}`)(client) })

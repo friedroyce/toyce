5ce2b21573cacdbf871d80736bb354f246c62dc9
Roy <friedroyze@gmail.com>
2022-06-08 18:18:58 +0800

    update meme.js
    changed variable name

--- src/commands/fun/meme.js
+++ src/commands/fun/meme.js
@@ -8,12 +8,13 @@ module.exports = {
     category: 'fun',
     aliases: ['meme'],
     async execute(client, command) {
-        let subreddits = ["memes"]
+        let subReddits = ["memes"]
 
-        let subreddit = subreddits[Math.floor(Math.random() * (subreddits.length))]
+        let subreddit = subReddits[Math.floor(Math.random() * (subReddits.length))]
         let advanced = await imageapi.advanced(subreddit, 'top')
 
         if(command.slash) await command.interaction.reply({ content: advanced.img, ephemeral: false });
         else command.message.channel.send(advanced.img)
+
     }
 }

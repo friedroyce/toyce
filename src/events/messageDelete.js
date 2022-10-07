5d4159597a70a421688be628ed5efd0628648c10
roy <friedroyze@gmail.com>
2022-10-07 17:41:39 +0800

    added message id array

--- src/events/messageDelete.js
+++ src/events/messageDelete.js
@@ -5,7 +5,7 @@ module.exports = {
     async execute(message, client){
         if (message.partial || (message.embeds.length && !message.content)) return; // content is null or deleted embed
 
-        client.snipes[message.channel.id] = {
+        client.snipes[message.channel.id][message.id] = {
             author: message.author,
             content: message.content,
             createdAt: message.createdTimestamp,
